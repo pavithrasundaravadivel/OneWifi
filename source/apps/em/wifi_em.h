2bcf736ece5cf09ad139966699a1850fe7ebd3f1
Pavithra Sundaravadivel <Pavithra_Sundaravadivel@Comcast.com>
2026-08-27 17:26:27 +0000

    Squashed commit of EM_XE2_XLE_OW_sync

--- source/apps/em/wifi_em.h
+++ source/apps/em/wifi_em.h
@@ -37,6 +37,7 @@ extern "C" {
 #define WIFI_SET_DISCONN_SCAN_NONE_STATE      "Device.WiFi.EM.SetDisconnScanNoneState"
 #define WIFI_EM_CLIENT_ASSOC_CTRL_REQ         "Device.WiFi.EM.ClientAssocCtrlRequest"
 #define WIFI_EM_FAILED_CONNECTION             "Device.WiFi.EM.FailedConnection"
+#define WIFI_EM_WEI_DATA                      "Device.WiFi.EM.WEIData"
 
 #define WNM_CATEGORY               10
 #define IEEE80211_HDRLEN_SM           24
@@ -93,6 +94,7 @@ typedef enum {
     em_app_event_type_ap_metrics_rad_chan_stats,
     em_app_event_type_assoc_dev_stats_periodic,
     em_app_event_type_vap_stats_periodic,
+    em_app_event_type_wei_data,
 
     em_app_event_type_max
 } em_app_event_type_t;
